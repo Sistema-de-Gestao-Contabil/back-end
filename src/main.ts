4b702e07a836d99fe1ea6fef1aac7e58fc45026b
Edson-Silva-22 <eds.17sv@gmail.com>
2023-11-14 21:22:15 -0300

    Adicionando cofiguração do CORS e validação de requisição

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  //O CORS é um mecanismo que permite que os recursos sejam solicitados de outro domínio
+  app.enableCors()
+
+  //Usando validação
+  app.useGlobalPipes(new ValidationPipe({
+    //Pega somente os campos da requisição que estão definidos no Dto e os que não estão, somente são descartados
+    whitelist: true,
+    //Semelhante ao whitelist porem ele lança uma excessão se houver algum campo na requisição que não está definido no Dto 
+    forbidNonWhitelisted: true
+
+  }))
+
+
+  await app.listen(8181);
 }
 bootstrap();
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -3,6 +3,6 @@ import { IsString } from 'class-validator';
 //Usando o class-validator para validar os dados que são enviados pelo front end atraves da requisição.
 export class CreateUserDto {
     //O campo name só sera validado se for uma string se não for sera retornado a messagem de erro que foi definida abixo
-    @IsString({message: "O nome de ser informado"})
+    @IsString({message: "O nome deve ser informado"})
     name: string
 }
